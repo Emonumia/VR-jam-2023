c242b842bb5c9c00813d7e6e9772d101d4527b47
Emonumia <117005254+Emonumia@users.noreply.github.com>
2023-06-29 13:17:56 +0200

    Save before merge

--- Assets/Scripts/CarControl.cs
+++ Assets/Scripts/CarControl.cs
@@ -1,59 +1,84 @@
-using System;
 using UnityEngine;
 
 public class CarControl : MonoBehaviour
 {
-    public WheelCollider leftWheel;
-    public WheelCollider rightWheel;
-    public float maxMotorTorque = 100f;
-    //public float maxSteeringAngle = 0f;
-    public GameObject left;
-    public GameObject right;
+    public WheelCollider fLeftWheel;
+    public WheelCollider fRightWheel;
+    public WheelCollider bLeftWheel;
+    public WheelCollider bRightWheel;
+    public Transform flWheel1;
+    public Transform frWheel2;
+    public Transform blWheel3;
+    public Transform brWheel4;
+    public float maxMotorTorque = 1000f;
+    public float maxSteeringAngle = 60f;
+    private float acc = 3;
+    public GameObject SteeringWheel;
 
-    private float leftInput = 0f;
-    private float rightInput = 0f;
+    public bool pedal_of_metal = false;
+    private float gasGasGas_i_gotta_step_on_the_gas = 0f;
 
     private void Start()
     {
-        leftWheel.motorTorque = 0;
-        rightWheel.motorTorque = 0;
+        fLeftWheel.motorTorque = 0f;
+        fRightWheel.motorTorque = 0f;
+        bLeftWheel.motorTorque = 0f;
+        bRightWheel.motorTorque = 0f;
+        fLeftWheel.steerAngle = 0f;
+        fRightWheel.steerAngle = 0f;
     }
 
     private void Update()
     {
-
-        if (Math.Abs(left.transform.rotation.eulerAngles.x -270) > 5 || Math.Abs(right.transform.rotation.eulerAngles.x -270) > 5)
+        if (pedal_of_metal)
         {
-            // Get input values from the levers
-            leftInput = left.transform.rotation.eulerAngles.x - 270;
-            rightInput = right.transform.rotation.eulerAngles.x - 270;
-        }
+            // Get input values from the X
+            gasGasGas_i_gotta_step_on_the_gas = acc;
 
-
-        if ( leftInput > 0 || rightInput > 0)
-        {
-            Debug.Log("Vroom");
-            Debug.Log(leftInput);
-            Debug.Log(rightInput);
         }
+        else
+            gasGasGas_i_gotta_step_on_the_gas = 0f;
     }
 
     private void FixedUpdate()
     {
-        // Control the left and right wheels separately
+        /* Control the left and right wheels separately
         float leftTorque = leftInput * maxMotorTorque * 0.1f;
-        float rightTorque = rightInput * maxMotorTorque * 0.1f;
+        float rightTorque = rightInput * maxMotorTorque * 0.1f;*/
 
         // Apply torque to the wheels
-        leftWheel.motorTorque = leftTorque;
-        rightWheel.motorTorque = rightTorque;
+        fLeftWheel.motorTorque = maxMotorTorque * gasGasGas_i_gotta_step_on_the_gas;
+        fRightWheel.motorTorque = maxMotorTorque * gasGasGas_i_gotta_step_on_the_gas;
+        //bLeftWheel.motorTorque = maxMotorTorque * gasGasGas_i_gotta_step_on_the_gas;
+        //bRightWheel.motorTorque = maxMotorTorque * gasGasGas_i_gotta_step_on_the_gas;
 
 
-        /*/ Control steering of the wheels
-        float leftSteeringAngle = leftInput * maxSteeringAngle;
-        float rightSteeringAngle = rightInput * maxSteeringAngle;
+        //Control steering of the wheels
+        float SteeringAngle = SteeringWheel.transform.rotation.eulerAngles.y;
+        Debug.Log(fLeftWheel.steerAngle);
+        fLeftWheel.steerAngle = SteeringAngle;
+        fRightWheel.steerAngle = SteeringAngle;
 
-        leftWheel.steerAngle = leftSteeringAngle;
-        rightWheel.steerAngle = rightSteeringAngle;*/
+        if (fLeftWheel.steerAngle != SteeringAngle || fRightWheel.steerAngle != SteeringAngle)
+        {
+            fLeftWheel.steerAngle = 0f;
+            fRightWheel.steerAngle = 0f;
+        }
+
+
+        UpWheels(fLeftWheel, flWheel1);
+        UpWheels(fRightWheel, frWheel2);
+
+        UpWheels(bLeftWheel, blWheel3);
+        UpWheels(bRightWheel, brWheel4);
+    }
+
+    void UpWheels(WheelCollider col, Transform trans)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        col.GetWorldPose(out pos, out rot);
+        trans.position = pos;
+        trans.rotation = rot;
     }
 }
